5bb9d2948bc3d632fed0850beb1febc60323dd3a
SeYearSt <jtik0s06@gmail.com>
2018-03-25 14:40:06 +0300

    made enter validation

--- lab_1/Association_and_symmetric_substraction.py
+++ lab_1/Association_and_symmetric_substraction.py
@@ -1,3 +1,5 @@
+import os.path
+
 #function for check if element exist in list
 def is_exist(el,list):
     for x in list:
@@ -5,33 +7,94 @@ def is_exist(el,list):
             return True
     return False
 
-#opening file in working directory
-lists_file = open('lists_file.txt','r')
 
+def get_united_set(A,B):
+    united_set = set()
+    for x in A:
+       united_set.add(x)
+    for x in B:
+        united_set.add(x)
+    return united_set
 
-first_set = {int(x) for x in lists_file.readline().split()}
-second_set = {int(x) for x in lists_file.readline().split()}
-lists_file.close()
-
-united_set = set()
-
-
-
-for x in first_set:
-    united_set.add(x)
-for x in second_set:
-    united_set.add(x)
+def get_symmetric_substraction_set(A,B):
+    symmetric_substraction_set = get_united_set(A,B)
     
-symmetric_substraction_set = {x for x in united_set}
-
-temp_list = [x for x in first_set if is_exist(x,second_set)]
-
-
-for x in temp_list:
-    symmetric_substraction_set.remove(x)
+    temp_list = [x for x in A if is_exist(x,B)]
     
-print("A =",first_set)
-print("B =",second_set)
-print("A(united)B =",united_set)
-print("common_elements = ",temp_list)
-print("symmetric substraction of sets A, B =",symmetric_substraction_set)
+    for x in temp_list:
+        symmetric_substraction_set.remove(x)
+    if len(symmetric_substraction_set) == 0:
+        return 0
+    else:
+        return symmetric_substraction_set
+    
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+    
+def get_int():
+    while True:
+        n = input("Input int: ")
+        if is_int(n):
+            return int(n)
+
+def get_list(char):
+    temp_set = set()
+    while True:
+        print("Enter size of array:")
+        n = get_int()
+        
+        if n >= 1:
+            break
+        
+    for i in range(n):
+        print(char,"[{}] = ".format(i+1),end='')
+        n = get_int()
+        temp_set.add(n)
+    return temp_set
+
+
+def get_lists_from_file(file_name):
+   A = set()
+   B = set()
+   if os.path.exists(file_name):
+       file = open(file_name,'r')
+       A = {int(x) for x in file.readline().split()}
+       #print(A)
+       B = {int(x) for x in file.readline().split()}
+       #print(B)
+       file.close()
+   else:
+        print("Fail to open file")
+   return A,B
+
+#file = input("Enter name of file: ")
+#print(os.path.exists(file))
+
+def main():
+    A = set()
+    B = set()
+    print("Enter 1 - input list from keyboard, 2 - input list from file")
+    while True:
+        control = get_int()
+        if control == 1:
+                   A = get_list('A')
+                   B = get_list('B')
+        elif control == 2 :
+                   file_name = input("Enter file name:")
+                   A, B = get_lists_from_file(file_name)
+                   if len(A) == 0 or len(B) == 0:
+                       print(A)
+                       print(B)
+                       print("End of program...")
+                       return;
+        if (control == 1 or control == 2):
+                   break
+   
+    print('A =',A)
+    print('B =', B)
+    print('A united B =',get_united_set(A,B))
+    print('A symmetric_substraction_set B =',get_symmetric_substraction_set(A,B))
